abf23b106201c0d37bd71d62750f899310775d96
Andrea Burattin <andrea.burattin@gmail.com>
2020-11-19 15:38:16 +0100

    Worked on download recording on ephemeral drive

--- src/main/java/pmcep/miners/recording_miner/RecordingMiner.java
+++ src/main/java/pmcep/miners/recording_miner/RecordingMiner.java
@@ -17,6 +17,7 @@ import pmcep.web.miner.models.MinerView;
 import javax.xml.transform.stream.StreamResult;
 import java.io.Console;
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -25,8 +26,11 @@ import java.util.*;
 
 import static javax.annotation.Resource.AuthenticationType.CONTAINER;
 
-@ExposedMiner(name = "Recording Miner", description = "This Miner is used for recording a stream", configurationParameters = {}, viewParameters = {
-		@ExposedMinerParameter(name = "File type", type = MinerParameter.Type.STRING) })
+@ExposedMiner(
+	name = "Recording Miner",
+	description = "This Miner is used for recording a stream",
+	configurationParameters = {},
+	viewParameters = {})
 public class RecordingMiner extends AbstractMiner {
 
 	private Set<String> attributes = new HashSet<>();
@@ -66,54 +70,46 @@ public class RecordingMiner extends AbstractMiner {
 	@Override
 	public List<MinerView> getViews(Collection<MinerParameterValue> collection) {
 		List<MinerView> views = new ArrayList<>();
-		for (MinerParameterValue minerParameterValue : collection) {
-			if (minerParameterValue.getName().equals("File type")) {
-				switch (String.valueOf(minerParameterValue.getValue())) {
-				case "XML":
-					String xmlFilePath = new XMLParser().convertToXML((HashMap<String, Trace>) caseMap);
-					String xmlLink = saveToCloud(xmlFilePath);
-					String htmlLink = "<a href=" + xmlLink + ">Download XML file here</a>";
-					views.add(new MinerView("Textual", htmlLink, MinerView.Type.RAW));
-					break;
-
-				}
-			}
-
-		}
+		try {
+			String xmlFilePath =  "test";//new XMLParser().convertToXML((HashMap<String, Trace>) caseMap);
+			String xmlLink = saveToCloud(xmlFilePath);
+			views.add(new MinerView("Download recording", xmlLink, MinerView.Type.BINARY));
+		} catch (Exception e) { }
 
 		return views;
 	}
 
-	public String saveToCloud(String xmlPath) {
+	public String saveToCloud(String xmlPath) throws IOException {
 
-		// Azure Connection string
-		String connectStr = "DefaultEndpointsProtocol=https;AccountName=opmframework;AccountKey=GZuLV1fA3apRDprLpZ/3kMCgiR8l6j9EhH+M88ncFB9xw91xXWveeEZbcJeBjCCIHJOk7+T6tcCh/E324x2dxg==;EndpointSuffix=core.windows.net";
-
-		BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectStr).buildClient();
-
-		// Unique container name
-		String containerName = "opm-recording" + java.util.UUID.randomUUID();
-		BlobContainerClient containerClient = blobServiceClient.createBlobContainer(containerName);
-		BlobSignedIdentifier identifier = new BlobSignedIdentifier().setId("name")
-				.setAccessPolicy(new BlobAccessPolicy().setStartsOn(OffsetDateTime.now())
-						.setExpiresOn(OffsetDateTime.now().plusDays(1)).setPermissions("r"));
-		try {
-			containerClient.setAccessPolicy(PublicAccessType.CONTAINER, Collections.singletonList(identifier));
-		} catch (UnsupportedOperationException err) {
-			System.out.printf("Set Access Policy failed because: %s\n", err);
-		}
-
-		Path path = Paths.get(xmlPath);
-		String fileName = path.getFileName().toString();
-
-		BlobClient blobClient = containerClient.getBlobClient(fileName);
-
-		blobClient.uploadFromFile(xmlPath);
-
-		// clean temp folder after
-		new File(path.toString()).delete();
-
-		return blobClient.getBlobUrl();
+		return "/files/" + FileServer.storeFile(xmlPath);
+//		// Azure Connection string
+//		String connectStr = "DefaultEndpointsProtocol=https;AccountName=opmframework;AccountKey=GZuLV1fA3apRDprLpZ/3kMCgiR8l6j9EhH+M88ncFB9xw91xXWveeEZbcJeBjCCIHJOk7+T6tcCh/E324x2dxg==;EndpointSuffix=core.windows.net";
+//
+//		BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectStr).buildClient();
+//
+//		// Unique container name
+//		String containerName = "opm-recording" + java.util.UUID.randomUUID();
+//		BlobContainerClient containerClient = blobServiceClient.createBlobContainer(containerName);
+//		BlobSignedIdentifier identifier = new BlobSignedIdentifier().setId("name")
+//				.setAccessPolicy(new BlobAccessPolicy().setStartsOn(OffsetDateTime.now())
+//						.setExpiresOn(OffsetDateTime.now().plusDays(1)).setPermissions("r"));
+//		try {
+//			containerClient.setAccessPolicy(PublicAccessType.CONTAINER, Collections.singletonList(identifier));
+//		} catch (UnsupportedOperationException err) {
+//			System.out.printf("Set Access Policy failed because: %s\n", err);
+//		}
+//
+//		Path path = Paths.get(xmlPath);
+//		String fileName = path.getFileName().toString();
+//
+//		BlobClient blobClient = containerClient.getBlobClient(fileName);
+//
+//		blobClient.uploadFromFile(xmlPath);
+//
+//		// clean temp folder after
+//		new File(path.toString()).delete();
+//
+//		return blobClient.getBlobUrl();
 
 	}
 
--- src/main/java/pmcep/web/MinerBackendApplication.java
+++ src/main/java/pmcep/web/MinerBackendApplication.java
@@ -2,9 +2,24 @@ package pmcep.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.Console;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.UUID;
 
 @SpringBootApplication
 public class MinerBackendApplication {
@@ -13,3 +28,42 @@ public class MinerBackendApplication {
 		SpringApplication.run(MinerBackendApplication.class, args);
 	}
 }
+
+
+@RestController
+@CrossOrigin
+class S {
+
+	
+//	public static String storeFile(String content) throws IOException {
+//		File tempFile = File.createTempFile(UUID.randomUUID().toString(), "");
+//		tempFile.deleteOnExit();
+//		
+//		Files.writeString(tempFile.toPath(), content, StandardCharsets.UTF_8);
+//		System.out.println(tempFile.getAbsolutePath());
+//		return tempFile.getName();
+//	}
+	
+	@GetMapping("/files/{fileId}")
+	public ResponseEntity<Resource> instanceStart(@PathVariable("fileId") String fileId) throws FileNotFoundException {
+		System.out.println("bla");
+		File f = new File(System.getProperty("java.io.tmpdir") + File.separator + fileId);
+		System.out.println(f.getAbsolutePath());
+		if (!f.exists()) {
+			return ResponseEntity.notFound().build();
+		}
+		InputStreamResource resource = new InputStreamResource(new FileInputStream(f));
+		
+//		if (!map.containsKey(fileId)) {
+//			return ResponseEntity.notFound().build();
+//		}
+//		File f = map.get(fileId);
+//		InputStreamResource resource = new InputStreamResource(new FileInputStream(f));
+//
+		return ResponseEntity
+				.ok()
+				.contentLength(f.length())
+				.body(resource);
+	}
+}
+
