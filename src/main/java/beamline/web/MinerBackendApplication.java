8f95b651af63a16eb418f2ac2cf778b065910214
Andrea Burattin <andrea.burattin@gmail.com>
2020-12-07 13:09:30 +0100

    Added proper dependencies scanning in main

--- src/main/java/beamline/web/MinerBackendApplication.java
+++ src/main/java/beamline/web/MinerBackendApplication.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@ComponentScan({"beamline.core"})
 public class MinerBackendApplication {
 
 	public static void main(String[] args) throws InterruptedException {
